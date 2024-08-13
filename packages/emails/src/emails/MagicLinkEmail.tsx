0ccfb931bacd4d87191df968b94bfbbe263b46ad
Waziaq <mr.zexr2010@gmail.com>
2024-08-13 11:21:48 +0800

    :pencil: Change the email header

--- packages/emails/src/emails/MagicLinkEmail.tsx
+++ packages/emails/src/emails/MagicLinkEmail.tsx
@@ -23,7 +23,7 @@ export const MagicLinkEmail = ({ url }: Props) => (
       <MjmlSection padding="0">
         <MjmlColumn>
           <HeroImage
-            src={`${env.NEXTAUTH_URL}/images/yourMagicLinkBanner.png`}
+            src={`${env.NEXTAUTH_URL}/images/yourMagicLinkBannerCustom.png`}
           />
         </MjmlColumn>
       </MjmlSection>
@@ -39,7 +39,7 @@ export const MagicLinkEmail = ({ url }: Props) => (
           </Text>
           <Text>
             Best,
-            <br />- Typebot Team.
+            <br />- Waapybot Team.
           </Text>
         </MjmlColumn>
       </MjmlSection>
